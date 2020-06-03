69d68d73364212efea1c686ae705fee009e9fb7b
Suman Kumar <sumancsnit@gmail.com>
2020-06-03 13:27:57 +0000

    initial setup repl.it

--- bigO.js
+++ bigO.js
@@ -1,1 +1,9 @@
-// test
+const names = ['nemo']
+
+const findNemo = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') console.log('nemo found')
+  }
+}
+
+findNemo(names)
