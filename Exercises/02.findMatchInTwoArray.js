67fd02a018917dbf13ea70992286bf7a4b7da020
Suman Kumar <sumancsnit@gmail.com>
2020-06-06 23:29:04 +0530

    new solution

---
+++ Exercises/02.findMatchInTwoArray.js
@@ -0,0 +1,11 @@
+// Given two array, create a function that let's a user know (true/false) wether these two arrays contain any common items.
+
+// example 
+const array1 = ['a', 'b', 'c', 'd', 'e']
+const array2 = ['w', 'x', 'y', 'a']
+// should return true 
+
+// example 
+const array1 = ['a', 'b', 'c', 'd', 'e']
+const array2 = ['w', 'x', 'y', 'z']
+// should return false
