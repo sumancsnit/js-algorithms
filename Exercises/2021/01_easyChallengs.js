747a7e51d8257d20080dd81a4b7d5322c9a3faa9
Suman Kumar <sumancsnit@gmail.com>
2021-08-02 23:14:04 +0530

    new exercise

--- Exercises/2021/01_easyChallengs.js
+++ Exercises/2021/01_easyChallengs.js
@@ -2,3 +2,6 @@
 // 01 sum numbers
 const sum = (...args) => args.reduce((accu, ele) => accu + ele);
 
+// 02 search substring from string
+
+const matchString = (string, subString) => string.includes(subString);
