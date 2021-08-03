68a957763ad80ca9ea501a2c01b72996c85a0a0c
Suman Kumar <sumancsnit@gmail.com>
2021-08-03 17:19:59 +0000

    find nemo

--- Exercises/2021/01_easyChallengs.js
+++ Exercises/2021/01_easyChallengs.js
@@ -5,3 +5,17 @@ const sum = (...args) => args.reduce((accu, ele) => accu + ele);
 // 02 search substring from string
 
 const matchString = (string, subString) => string.includes(subString);
+
+// 03 find nemo 
+
+const nemo = ['nemo'];
+
+const findNemo = (array) => {
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === 'nemo'){
+      console.log('nemo found')
+    }
+  }
+}
+
+findNemo(nemo)
