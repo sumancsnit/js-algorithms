e2134a53a45acd71bf1f243f96021408ca62e6e1
suman kumar <sumancsnit@gmail.com>
2024-05-01 20:46:22 +0530

    feat: linked list pop

--- 2024/linkedList.js
+++ 2024/linkedList.js
@@ -25,10 +25,30 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    // for empty list
+    if (!this.head) return;
+    let current = this.head;
+    let pre = this.head;
+    while (current.next) {
+      pre = current;
+      current = current.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    // if length is one
+    if (!this.length) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
-let myLinkedList = new LinkedList(4);
-myLinkedList.push(5);
-// myLinkedList.push(6)
-console.log(myLinkedList.push(7));
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+console.log(myLinkedList.pop());
 console.log(myLinkedList);
